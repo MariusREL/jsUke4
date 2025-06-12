36a85ff7fe87d9f297f53304c93c3e86683edffa
MariusREL <mrel0425@gmail.com>
2025-06-12 21:05:38 +0200

    finish unit converter and add kittens

--- index.js
+++ index.js
@@ -179,3 +179,58 @@ elements.array.buttons.clear.addEventListener("click", (e) => {
     }
     updateArray();
 });
+
+
+const inputText = document.querySelector(".input")
+const convertBtn = document.querySelector(".convert-btn")
+const lengthPara = document.querySelector("#length-conversion")
+const volumePara = document.querySelector("#volume-conversion")
+const massPara = document.querySelector("#mass-conversion")
+
+inputText.addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+        e.preventDefault(); 
+        convertBtn.click(); 
+    }
+});
+
+function meterToFeet(a){
+     return (a * 3.28084).toFixed(2)
+}
+function feet(a){
+    return (a * 0.3048).toFixed(2)
+}
+
+function literToGal(a){
+    return (a * 3.78541).toFixed(2)
+}
+
+function galToLiters(a){
+    return (a * 0.264172).toFixed(2)
+}
+
+function kiloToPounds(a){
+    return (a * 2.20462).toFixed(2)
+}
+
+function poundsToKilo(a){
+    return (a / 2.20462).toFixed(2)
+}
+convertBtn.addEventListener("click", function(){
+    const userInput = Number(inputText.innerText);
+    if (!isNaN(userInput)){
+
+        lengthPara.innerHTML = `${userInput} meters = ${meterToFeet(userInput)} feet | ${userInput} feet = ${feet(userInput)} meters  `;
+
+        volumePara.innerHTML = `${userInput} liters = ${literToGal(userInput)} gallons | ${userInput} gallons = ${galToLiters(userInput)} liters  `;
+
+        massPara.innerHTML = `${userInput} kilos = ${kiloToPounds(userInput)} pounds | ${userInput} pounds = ${poundsToKilo(userInput)} kilos  `;
+
+    } else {
+        lengthPara.innerHTML = `That's no number laddie `;
+        volumePara.innerHTML = `You should type a number instead`
+        massPara.innerHTML = `Stop being difficult `
+    
+    }   
+})
+
